bae605a22fe2f9e2936e950ff0dbb17fde1f766b
Roberto <matium2323@gmail.com>
2024-11-05 07:32:05 -0300

    Adicionado o padrao ViewModel

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repository.Interface;
+using LanchesMac.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 /*Se por acaso ouvre erro na hora de exibir a view, verificar a versao do entetyFrameWork 
@@ -17,8 +18,15 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _repository.Lanches;
-            return View(lanches);
+        
+            var lancheListViewModel = new LancheListViewModel();
+
+            // O lanche List view model recebera os valores vindo da model que serao exibidos na view
+            lancheListViewModel.Lanches = _repository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+
+            return View(lancheListViewModel);
         }
     }
 }
--- LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f3d6f815cb4078ff3bdf79b878120588bd2f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
